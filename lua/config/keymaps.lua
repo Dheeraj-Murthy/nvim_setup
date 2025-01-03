22b0dba192854bc9b756f1b2cab5b9f78d49651b
meenigam <ms.dheerajmurthy@iiitb.ac.in>
2025-01-03 23:46:49 +0530

    integrated ghostty setup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,14 +2,17 @@
 
 -- this is the command for command enter. escape into a new line from withing a line
 vim.keymap.set("n", "696969", "<Esc>o", { noremap = true, silent = true })
--- this is the keymap for command del. I have already set up del to return hex code 0x15 in iterm2 setting so that corrensponds to ctrl u this command just deletes the entire line
+vim.keymap.set("n", "<D-CR>", "<Esc>o", { noremap = true, silent = true })
+-- this is the keymap for command del. I have already set up del to return hex code 0x16 in iterm2 setting so that corrensponds to ctrl u this command just deletes the entire line
 vim.keymap.set("n", "<C-u>", ":<C-u>normal! dd<CR>", { noremap = true, silent = true })
 -- this is the key map to set command-s to save the current file
-vim.keymap.set({ "n", "i" }, "696970", ":w<CR>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "i" }, "696970", "<Esc>:w<CR>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "i" }, "<D-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
 -- this is the select all on pressing cmd-a keymap
 vim.keymap.set({ "i", "n" }, "696971", "ggVG", { noremap = true, silent = true })
+vim.keymap.set({ "i", "n" }, "<D-a>", "ggVG", { noremap = true, silent = true })
 -- this is the keymap to remove the delete functionality of ctrl - u and make it go half page up
-vim.keymap.set("n", "42069", "<C-u>", { noremap = true, silent = true, desc = "" })
+vim.keymap.set("n", "42070", "<C-u>", { noremap = true, silent = true, desc = "" })
 -- this is the command to set nvim to paste only from the yank buffer
 vim.api.nvim_set_keymap("v", "p", '"_dp', { noremap = true, silent = true, desc = "continnual paste" })
 
