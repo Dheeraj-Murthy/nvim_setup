10fdd78e6001c0000e6d929fd35006bb23846f9b
meenigam <ms.dheerajmurthy@iiitb.ac.in>
2025-02-02 15:41:41 +0530

    correct theme not loading error

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,9 +2,10 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.api.nvim_create_autocmd("BufReadPre", {
-	callback = function()
-		if vim.fn.line("$") > 5000 then
-			vim.cmd("TSBufDisable highlight")
-		end
-	end,
+  callback = function()
+    if vim.fn.line("$") > 5000 then
+      vim.cmd("TSBufDisable highlight")
+    end
+  end,
 })
+vim.cmd("colorscheme tokyonight")
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,7 +10,7 @@
 return {
   "nvim-lualine/lualine.nvim",
   lazy = false,
-  dependencies = { "nvim-tree/nvim-web-devicons" },   -- Optional for icons
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- Optional for icons
   config = function()
     require("lualine").setup({
       options = {
@@ -21,22 +21,22 @@ return {
         disabled_filetypes = { "NvimTree", "dashboard", "packer" }, -- Disable lualine for certain filetypes
       },
       sections = {
-        lualine_a = { "mode" },                                       -- Display mode in the first section
-        lualine_b = { "branch", "diff", "diagnostics" },              -- Branch, git diff, diagnostics in the second section
-        lualine_c = { "filename", "filetype" },                       -- Show filename and filetype in the third section
-        lualine_x = { "encoding", "fileformat", "filetype" },         -- File encoding, format, and type in the fourth section
-        lualine_y = { "progress" },                                   -- Show progress (e.g., percentage of file)
-        lualine_z = { "location" },                                   -- Display cursor position
+        lualine_a = { "mode" },                               -- Display mode in the first section
+        lualine_b = { "branch", "diff", "diagnostics" },      -- Branch, git diff, diagnostics in the second section
+        lualine_c = { "filename", "filetype" },               -- Show filename and filetype in the third section
+        lualine_x = { "encoding", "fileformat", "filetype" }, -- File encoding, format, and type in the fourth section
+        lualine_y = { "progress" },                           -- Show progress (e.g., percentage of file)
+        lualine_z = { "location" },                           -- Display cursor position
       },
       inactive_sections = {
-        lualine_a = { "filename" },         -- Show the filename only when inactive
+        lualine_a = { "filename" }, -- Show the filename only when inactive
         lualine_b = {},
         lualine_c = {},
         lualine_x = {},
         lualine_y = {},
         lualine_z = {},
       },
-      extensions = { "fugitive" },       -- Add extensions like 'fugitive' for git integration
+      extensions = { "fugitive" }, -- Add extensions like 'fugitive' for git integration
     })
   end,
 }
