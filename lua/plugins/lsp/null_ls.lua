8af2e134717a21e43b9a1c6f33dbdf597993108f
meenigam <ms.dheerajmurthy@iiitb.ac.in>
2025-08-28 12:59:49 +0530

    added bst snippet

--- lua/plugins/lsp/null_ls.lua
+++ lua/plugins/lsp/null_ls.lua
@@ -1,60 +1,65 @@
 return {
     "nvimtools/none-ls.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    ft = { "python", "markdown", "sql" }, -- added sql here
+    ft = { "python", "markdown", "sql", "html", "css", "javascript", "typescript" },
     config = function()
         local null_ls = require("null-ls")
 
-        -- Define your sources for Python, Markdown, and SQL
-        local python_sources = {
-            null_ls.builtins.formatting.black,
-            null_ls.builtins.formatting.isort,
-            null_ls.builtins.diagnostics.pylint,
-        }
-        local markdown_sources = {
-            null_ls.builtins.formatting.prettier.with({
-                filetypes = { "markdown" },
-            }),
-        }
-        local sql_sources = {
-            null_ls.builtins.formatting.sqlfluff.with({
-                extra_args = { "--dialect", "mysql" }, -- or postgres, depending on what you use
-            }),
-        }
+        local formatting = null_ls.builtins.formatting
+        local diagnostics = null_ls.builtins.diagnostics
 
-        -- Combine sources into one flat list
-        local all_sources = {}
-        for _, source in ipairs(python_sources) do
-            table.insert(all_sources, source)
-        end
-        for _, source in ipairs(markdown_sources) do
-            table.insert(all_sources, source)
-        end
-        for _, source in ipairs(sql_sources) do
-            table.insert(all_sources, source)
-        end
+        -- Define all formatters and linters
+        local sources = {
+            -- Python
+            formatting.black,
+            formatting.isort,
+            diagnostics.pylint,
+
+            -- Markdown + Prettier-compatible
+            formatting.prettier.with({
+                filetypes = {
+                    "markdown",
+                    "html",
+                    "css",
+                    "javascript",
+                    "javascriptreact",
+                    "typescript",
+                    "typescriptreact",
+                    "json",
+                    "yaml",
+                },
+            }),
+
+            -- SQL
+            formatting.sqlfluff.with({
+                extra_args = { "--dialect", "mysql" },
+            }),
+        }
 
         null_ls.setup({
-            sources = all_sources,
+            sources = sources,
             on_attach = function(client, bufnr)
                 if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_buf_set_keymap(
-                        bufnr,
-                        "n",
-                        "<leader>f",
-                        "<cmd>lua vim.lsp.buf.format({ async = true })<CR>",
-                        { noremap = true, silent = true }
-                    )
-
+                    -- Format on save
                     vim.api.nvim_create_autocmd("BufWritePre", {
                         buffer = bufnr,
                         callback = function()
                             vim.lsp.buf.format({ async = false })
-                            vim.opt_local.wrap = true
-                            vim.opt_local.linebreak = true
-                            vim.opt_local.spell = true
+
+                            -- Optional extras for markdown
+                            local ft = vim.bo[bufnr].filetype
+                            if ft == "markdown" then
+                                vim.opt_local.wrap = true
+                                vim.opt_local.linebreak = true
+                                vim.opt_local.spell = true
+                            end
                         end,
                     })
+
+                    -- Manual format key
+                    vim.keymap.set("n", "<leader>f", function()
+                        vim.lsp.buf.format({ async = true })
+                    end, { buffer = bufnr, noremap = true, silent = true, desc = "Format file" })
                 end
             end,
         })
