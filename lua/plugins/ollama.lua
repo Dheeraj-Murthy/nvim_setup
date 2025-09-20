5484684dbc46985439e8e65e6592a8a64fdeae54
meenigam <ms.dheerajmurthy@iiitb.ac.in>
2025-09-20 22:07:14 +0530

    changed default llm to codellama 13b with km optimisation

--- lua/plugins/ollama.lua
+++ lua/plugins/ollama.lua
@@ -1,9 +1,10 @@
 return {
     "Dheeraj-Murthy/Ollama_chat.nvim",
+    -- dir = "~/Dev/nvim_plugin/ollama_chat.nvim",
     enabled = true,
     config = function()
         require("ollamarun").setup({
-            model = "codellama:7b-instruct-q4_0", -- or any other Ollama model
+            model = "codellama:13b-instruct-q4_k_m ", -- or any other Ollama model
         })
     end
 }
